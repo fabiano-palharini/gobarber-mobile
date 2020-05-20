615b08d4ba09fcb24240a25999749af55a1bcb78
Fabiano Palharini <fabianofalco@gmail.com>
2020-05-19 23:28:53 -0300

    SignIn screen

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Image } from 'react-native';
 import { Container, Title } from './styles';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
 import logoImg from '../../assets/logo.png';
 
 const SignIn: React.FC = () => {
@@ -8,6 +10,16 @@ const SignIn: React.FC = () => {
     <Container>
       <Image source={logoImg} />
       <Title>Logon</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Password" />
+      <Button
+        onPress={() => {
+          console.log('test');
+        }}
+      >
+        Sign in
+      </Button>
     </Container>
   );
 };
